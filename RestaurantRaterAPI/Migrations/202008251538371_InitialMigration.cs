78dcdd6fd4a0bbd22ea4fadc444a11f2ad2c61c7
bmagee2 <bamagee2@gmail.com>
2020-08-25 12:00:34 -0400

    more

---
+++ RestaurantRaterAPI/Migrations/202008251538371_InitialMigration.cs
@@ -0,0 +1,28 @@
+﻿namespace RestaurantRaterAPI.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialMigration : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Restaurants",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(nullable: false),
+                        Rating = c.Double(nullable: false),
+                        // No IsRecommended because it's a read only property
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Restaurants");
+        }
+    }
+}
---
+++ RestaurantRaterAPI/Models/RestaurantDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace RestaurantRaterAPI.Models
+{
+    public class RestaurantDbContext : DbContext    // Context
+    {
+                                         // Name "DefaultConnection" from Connection String in Web.config
+        public RestaurantDbContext() : base("DefaultConnection") { }  // BASE CONSTRUCTOR
+
+                // whole set of Restaurants -- table in the db
+        public DbSet<Restaurant> Restaurants { get; set; }
+
+    }
+}
